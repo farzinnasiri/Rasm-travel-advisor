13ae2609f0ac61538c8f856f7ffce780668a13b9
farzinnasiri <farzin-nasiri@hotmail.com>
2019-09-19 21:41:36 +0430

    options list added

--- app/src/main/java/com/rasm/MainActivity.java
+++ app/src/main/java/com/rasm/MainActivity.java
@@ -3,6 +3,8 @@ package com.rasm;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +15,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.ImageView;
+
+import com.rasm.adapters.OptionsRecyclerViewAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +29,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initRecyclerViewers();
+
     }
 
-    @Override
-    public void onBackPressed() {
+    private void initRecyclerViewers() {
+
+        ArrayList<String> titles = new ArrayList<>();
+        titles.add("ماجراجویی");
+        titles.add("تنظیمات");
+        titles.add("درباره ما");
+        ArrayList<Integer> optionsImages = new ArrayList<>();
+        optionsImages.add(R.drawable.adventures);
+        optionsImages.add(R.drawable.ic_settings);
+        optionsImages.add(R.drawable.ic_info);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = findViewById(R.id.options_list);
+        recyclerView.setLayoutManager(layoutManager);
+        OptionsRecyclerViewAdapter adapter = new OptionsRecyclerViewAdapter(this, titles,
+                optionsImages);
+        recyclerView.setAdapter(adapter);
 
     }
 
