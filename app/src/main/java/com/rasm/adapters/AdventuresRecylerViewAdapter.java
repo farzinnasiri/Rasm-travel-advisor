70939eed1adc58d9d293615e09677f9a02aa2fb0
farzinnasiri <farzin-nasiri@hotmail.com>
2019-09-20 14:03:56 +0430

    adventures adapter added

--- app/src/main/java/com/rasm/adapters/AdventuresRecylerViewAdapter.java
+++ app/src/main/java/com/rasm/adapters/AdventuresRecylerViewAdapter.java
@@ -1,22 +1,37 @@
 package com.rasm.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.rasm.R;
+import com.rasm.adventures.Adventure;
+
+import java.util.ArrayList;
 
 public class AdventuresRecylerViewAdapter extends
         RecyclerView.Adapter<AdventuresRecylerViewAdapter.ViewHolder> {
 
+    private ArrayList<Adventure> adventures;
+    private Context mContext;
 
+    public AdventuresRecylerViewAdapter(Context mContext, ArrayList<Adventure> adventures){
+
+
+    }
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adventure_item,
+                parent, false);
+        return new AdventuresRecylerViewAdapter.ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int positon) {
 
     }
 
