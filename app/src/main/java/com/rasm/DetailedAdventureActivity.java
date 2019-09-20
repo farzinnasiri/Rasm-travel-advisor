e6482b938002c26781f3d76cd53537d1ae4ce20a
farzinnasiri <farzin-nasiri@hotmail.com>
2019-09-20 19:27:42 +0430

    detailed adventure layout added

---
+++ app/src/main/java/com/rasm/DetailedAdventureActivity.java
@@ -0,0 +1,13 @@
+package com.rasm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedAdventureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_adventure);
+    }
+}
